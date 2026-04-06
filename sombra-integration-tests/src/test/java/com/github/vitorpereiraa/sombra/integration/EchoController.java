700a4d501267c182e2305b9c17b1317d169ab3ec
Vitor Pereira <vitor.cbs.pereira@gmail.com>
2026-04-06 12:21:04 +0100

    rewrite ReplayIT as true e2e test

--- sombra-integration-tests/src/test/java/com/github/vitorpereiraa/sombra/integration/EchoController.java
+++ sombra-integration-tests/src/test/java/com/github/vitorpereiraa/sombra/integration/EchoController.java
@@ -1,6 +1,8 @@
 package com.github.vitorpereiraa.sombra.integration;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class EchoController {
 
+    private final AtomicInteger callCount = new AtomicInteger();
+
     @PostMapping("/echo")
     ResponseEntity<String> echo(@RequestBody String body) {
+        callCount.incrementAndGet();
         return ResponseEntity.ok(body);
+    }
+
+    @GetMapping("/echo/count")
+    ResponseEntity<Integer> count() {
+        return ResponseEntity.ok(callCount.get());
     }
 }
